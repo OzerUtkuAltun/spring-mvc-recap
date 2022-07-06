668e89a7401b0f08eb447a2200937a5c5f7dfbab
Özer Altun <ozerutkualtun@gmail.com>
2022-07-06 15:25:00 +0300

    Bidnding request params

--- src/main/java/com/ozerutkualtun/mvc/spring_mvc_recap/controller/NameFormController.java
+++ src/main/java/com/ozerutkualtun/mvc/spring_mvc_recap/controller/NameFormController.java
@@ -3,6 +3,7 @@ package com.ozerutkualtun.mvc.spring_mvc_recap.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Locale;
@@ -28,6 +29,14 @@ public class NameFormController {
         model.addAttribute("message", name.toUpperCase(Locale.ROOT));
         return "name-form-result";
     }
+
+    @RequestMapping("/processNameFormFromModelUsingRequestParam")
+    public String processNameFormFromModel(@RequestParam("studentName") String name, Model model) {
+
+        model.addAttribute("message", name.toUpperCase(Locale.ROOT) + " (message read by request param)");
+        return "name-form-result";
+    }
+
 
 
 }
