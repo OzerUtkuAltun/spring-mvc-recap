250e776b9f77a5ebb8028bdfed1d1fcb369dc81d
Özer Altun <ozerutkualtun@gmail.com>
2022-07-07 11:21:40 +0300

    Handling string input for string fields (advanced)

--- src/main/java/com/ozerutkualtun/mvc/spring_mvc_recap/config/DemoAppConfig.java
+++ src/main/java/com/ozerutkualtun/mvc/spring_mvc_recap/config/DemoAppConfig.java
@@ -1,8 +1,12 @@
 package com.ozerutkualtun.mvc.spring_mvc_recap.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -23,6 +27,21 @@ public class DemoAppConfig {
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("message"); // direkt bu şekilde vermek gerekiyor ismi.
+        resourceBundleMessageSource.setDefaultEncoding("UTF-8");
+        return resourceBundleMessageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
     }
 
 }
